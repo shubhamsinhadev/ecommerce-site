251f48221814a300d45532a68ecb26435e86a445
shubhamsinhadev <sinha_shubham@rocketmail.com>
2025-01-21 01:37:20 +0530

    add react-router, react-query, chakraui

---
+++ client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router";
+
+export default function Navbar() {
+  return (
+    <>
+      <div>Navbar</div>
+      <Outlet />
+    </>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Provider } from "@/components/ui/provider";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
