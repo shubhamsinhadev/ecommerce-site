98b8e37e96cb9ed07eb5415820da9f2cb3ea06d2
shubhamsinhadev <sinha_shubham@rocketmail.com>
2025-01-25 06:03:15 +0530

    cart price details added

--- client/src/components/cart/CartDisplay.tsx
+++ client/src/components/cart/CartDisplay.tsx
@@ -2,13 +2,14 @@ import { useFetchCart } from "@/hooks/cart";
 import CartEmpty from "./CartEmpty";
 import { Box } from "@chakra-ui/react";
 import CartCard from "./CartCard";
+import CartPrice from "./CartPrice";
 export default function CartDisplay() {
   const { isPending, isError, error, data } = useFetchCart();
 
   if (isPending) return <div>Loading...</div>;
   if (isError) return <div>Error: {error.message}</div>;
 
-  if (!data) return <div>No items in the cart</div>;
+  if (!data) return <div>Error in Cart Data</div>;
 
   if (data.length === 0) return <CartEmpty />;
 
@@ -27,6 +28,7 @@ export default function CartDisplay() {
       {data.map((i) => {
         return <CartCard key={i._id} i={i} />;
       })}
+      <CartPrice />
     </Box>
   );
 }
---
+++ client/src/components/cart/CartPrice.tsx
@@ -0,0 +1,72 @@
+import { useAppSelector } from "@/redux/hooks";
+import { Card, Flex, Separator, Text } from "@chakra-ui/react";
+import { InfoTip } from "../ui/toggle-tip";
+import { ReactNode } from "react";
+
+type IList = { label: string; value: number; content?: ReactNode };
+
+export default function CartPrice() {
+  const data = useAppSelector((state) => state.cart);
+
+  const price = data.reduce((acc: number, { product, quantity }) => {
+    acc += product.price * quantity;
+    return acc;
+  }, 0);
+
+  const List: IList[] = [
+    { label: "Subtotal", value: price },
+    {
+      label: "Shipping",
+      value: 50,
+      content: (
+        <Text p={1}>
+          Flat{" "}
+          {new Intl.NumberFormat("en-IN", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0,
+          }).format(50)}
+        </Text>
+      ),
+    },
+    {
+      label: "Taxes",
+      value: price * 0.1,
+      content: <Text p={1}>10% GST</Text>,
+    },
+    { label: "Total Amount", value: price * 1.1 + 50 },
+  ];
+
+  return (
+    <Card.Root
+      bgColor={"white"}
+      maxW={"md"}
+      w={"100%"}
+      p={4}
+      shadow={"sm"}
+      display={"flex"}
+      flexDir={"column"}
+      gap={4}
+    >
+      {List.map(({ label, value, content }, idx) => (
+        <>
+          <Flex alignItems={"center"} key={label}>
+            <Text color={"gray.800"}>{label}</Text>
+            {content && <InfoTip content={content} />}
+            <div style={{ flex: 1 }}></div>
+            <Text fontWeight={"bold"} fontSize={"md"}>
+              {new Intl.NumberFormat("en-IN", {
+                style: "currency",
+                currency: "USD",
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              }).format(value)}
+            </Text>
+          </Flex>
+          {idx == 2 && <Separator mx={-4} />}
+        </>
+      ))}
+    </Card.Root>
+  );
+}
